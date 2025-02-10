8379f068267098321a63ac0c1fd598e98ba6ab43
kaichi0michael <ihciak.0127@gmail.com>
2025-02-11 08:01:28 +0900

    画像の変更完

--- jQuery_resize/main.js
+++ jQuery_resize/main.js
@@ -1,4 +1,10 @@
 $(window).resize(function() {
+	timer = setTimeout(function() {
+		width_disp();
+	}, 200);
+});
+
+$(window).resize(function() {
 	width_disp();
 });
  
@@ -7,13 +13,15 @@ $(document).ready(function() {
 });
  
 function width_disp() {
+    let img = $("#resize-img");
+
 	if (window.innerWidth > 1000) {
-		$("p").text("画面幅は1000ピクセル以上です");
+        img.attr("src", "img/Descktop.png");
 	} else if (window.innerWidth > 800) {
-		$("p").text("画面幅は800ピクセル以上です");
+        img.attr("src", "img/Laptop.png");
 	} else if (window.innerWidth > 600) {
-		$("p").text("画面幅は600ピクセル以上です");
+        img.attr("src", "img/iPad.png");
 	} else {
-		$("p").text("画面幅は600ピクセルより小さいです。");
+        img.attr("src", "img/SmartPhone.png");
 	}
 }
