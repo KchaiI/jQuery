3de2a0e4975de9ff9b8744a394fe0848ab1af324
kaichi0michael <ihciak.0127@gmail.com>
2025-02-12 17:59:13 +0900

    comment add

--- jQuery_resize/main.js
+++ jQuery_resize/main.js
@@ -16,12 +16,16 @@ function width_disp() {
     let img = $("#resize-img");
 
 	if (window.innerWidth > 1000) {
+        $(".comment").text("画面幅は1000ピクセル以上です");
         img.attr("src", "img/Descktop.png");
-	} else if (window.innerWidth > 800) {
+	} else if (window.innerWidth > 800 || window.innerWidth <= 1000) {
+        $(".comment").text("画面幅は800ピクセル以上です");
         img.attr("src", "img/Laptop.png");
-	} else if (window.innerWidth > 600) {
+	} else if (window.innerWidth > 600 || window.innerWidth <= 800) {
+        $(".comment").text("画面幅は600ピクセル以上です");
         img.attr("src", "img/iPad.png");
 	} else {
+        $(".comment").text("画面幅は600ピクセルより小さいです。");
         img.attr("src", "img/SmartPhone.png");
 	}
 }
