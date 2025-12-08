d4d678e5a5944ed0ebd03b256099b63bea22b484
kaichi0michael <ihciak.0127@gmail.com>
2025-12-08 15:09:51 +0900

    ハンバーガーメニュー実装

--- hanbager-menu/main.js
+++ hanbager-menu/main.js
@@ -1,6 +1,7 @@
 $(function(){
-  $('.btn-trigger').on('click', function() {
-    $(this).toggleClass('active');
+  $('.hamburger_button').on('click', function() {
+    $(this).toggleClass('active');     // ハンバーガーを×に
+    $('#js-nav').toggleClass('active'); // メニュー表示ON/OFF
     return false;
   });
 });
